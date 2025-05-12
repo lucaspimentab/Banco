ff551b847d0ccb91628a8cc3788f3348a956f50f
Mateus <mr.lordblader@gmail.com>
2025-05-12 14:11:14 -0300

    Cria classe CampoComIcone e a classe Tela

--- src/interface/componentes/login.py
+++ src/interface/componentes/login.py
@@ -1,39 +1,29 @@
 import flet as ft
+from interface.componentes.campo_com_icone import CampoComIcone
 
 class TelaLogin:
     def __init__(self, on_login_callback=None):
         self.on_login_callback = on_login_callback
+        
+        # Definindo as referências para os campos de entrada:
+        self.login_ref = ft.Ref[str]()
+        self.senha_ref = ft.Ref[str]()
+
         self.view = self.criar_view()
 
     def criar_view(self):
-        login_text = ft.Text("Login", size=24, weight=ft.FontWeight.BOLD)
 
-        login_row = ft.Row(
-            controls=[
-                ft.Icon(ft.Icons.PERSON),
-                ft.TextField(label="Usuário", autofocus=True, ref=ft.Ref[str]())
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        )
-
-        password_row = ft.Row(
-            controls=[
-                ft.Icon(ft.Icons.LOCK),
-                ft.TextField(label="Senha", password=True, can_reveal_password=True, ref=ft.Ref[str]())
-            ],
-            alignment=ft.MainAxisAlignment.CENTER
-        )
-
+        # Subitens da tela de login:
+        titulo = ft.Text("Login", size=24, weight=ft.FontWeight.BOLD)
+        login_row = CampoComIcone("person", "Usuário", ref_obj=self.login_ref)
+        password_row = CampoComIcone("lock", "Senha", senha=True, ref_obj=self.senha_ref)
         login_button = ft.ElevatedButton("Entrar", on_click=self.on_login_click)
         register_link = ft.Text("Não tem uma conta? Cadastre-se", color=ft.Colors.BLUE_500)
-
-        self.login_input = login_row.controls[1]
-        self.password_input = password_row.controls[1]
-
+        
         return ft.Container(
             content=ft.Column(
                 controls=[
-                    login_text,
+                    titulo,
                     login_row,
                     password_row,
                     login_button,
@@ -48,6 +38,6 @@ class TelaLogin:
 
     def on_login_click(self, e):
         print("Usuário:", self.login_input.value)
-        print("Senha:", self.password_input.value)
+        print("Senha:", self.senha_input.value)
         if self.on_login_callback:
-            self.on_login_callback(self.login_input.value, self.password_input.value)
+            self.on_login_callback(self.login_input.value, self.senha_input.value)
